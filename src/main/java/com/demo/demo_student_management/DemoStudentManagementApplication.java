6342f67572c18734eba93788fd4ef0bd86b90eee
tphgggg <buituanphong8@gmail.com>
2024-07-01 17:34:04 +0700

    first commit

---
+++ src/main/java/com/demo/demo_student_management/DemoStudentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.demo.demo_student_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoStudentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoStudentManagementApplication.class, args);
+	}
+
+}
